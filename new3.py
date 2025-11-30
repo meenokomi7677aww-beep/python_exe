53c7e3ee8700cfe7afcea704d4994a220f2b76e6
Rip_CuteBacon_Is_God <minthway2008@gmail.com>
2025-11-30 11:49:19 +0630

    add new 3.py

---
+++ new3.py
@@ -0,0 +1,13 @@
+num1 = input("Enter number1 \n")
+num2 = input("Enter number2 \n")
+
+
+add = int(num1)+int(num2)
+sub = int(num1)-int(num2)
+multi =int(num1)*int(num2)
+div = int(num1)/int(num2)
+
+print(f"your addition answer is {add}.")
+print(f"your subtraction answer is {sub}.")
+print(f"your multiplication answer is {multi}.")
+print(f"your division answer is {div}.")
